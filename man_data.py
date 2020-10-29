f5c0874455f97f23ef7c77cbf08f212b385fcfb6
Ian Nery Bandeira <ian.muxis@gmail.com>
2020-10-29 18:35:31 -0300

    preparado pra mandar os dados pra plot.js, que não existe ainda

--- man_data.py
+++ man_data.py
@@ -21,43 +21,56 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import classification_report, roc_auc_score
 from category_encoders import *
 
+import scikitplot as skplt
+import matplotlib.pyplot as plt
+
 
 class Manipulador_de_Dados():
     def __init__(self):
         self.data = pd.read_excel('dataset.xlsx')
         self.random_state = 25
+        self.samples = 6
+        self.estimators = 500
+        self.sample_per_leaf = 10
+        
     def init_algo(self):
         self.filter_data()
         self.random_forest()
+        return self.data_for_plot, self.data_for_shap
+    
     def filter_data(self):
         self.data['SARS-Cov-2 exam result'] = self.data['SARS-Cov-2 exam result'].map(dict(negative=0, positive=1))
         self.data['Urine - pH'] = self.data['Urine - pH'].map(lambda x: -100 if isinstance(x, str) else x).astype(float)
         self.data['Urine - Leukocytes'] = self.data['Urine - Leukocytes'].map(lambda x: 500 if x == '<1000' else x).astype(float)
         self.exams = self.data.drop(['Patient ID', 
-                               'Patient age quantile', 
                                'SARS-Cov-2 exam result', 
                                'Patient addmited to regular ward (1=yes, 0=no)', 
                                'Patient addmited to semi-intensive unit (1=yes, 0=no)', 
                                'Patient addmited to intensive care unit (1=yes, 0=no)'], axis = 1)
-        self.exams['Patient age quantile'] = self.data['Patient age quantile']
+        
     def random_forest(self):
-        for tr, ts in KFold(n_splits=4, shuffle=True, random_state=self.random_state).split(self.exams):
-            exams_training = self.exams.iloc[tr]
-            exams_test_set = self.exams.iloc[ts]
+        iterator = 0
+        for tr, ts in KFold(n_splits=self.samples).split(self.exams):
+            iterator += 1
+            print("Rodando a random forest para sample", iterator, "de", self.samples, "...")
             y_training = self.data.iloc[tr]['SARS-Cov-2 exam result']
             y_test_set = self.data.iloc[ts]['SARS-Cov-2 exam result']
+            exams_training = self.exams.iloc[tr]
+            exams_test_set = self.exams.iloc[ts]
             
             exams_training, exams_test_set = self.encode_fill_NaN(exams_training, exams_test_set)
 
-            rforest_model = RandomForestClassifier(random_state=self.random_state,
+            rforest_model = RandomForestClassifier(
                                         criterion='gini',
-                                        n_estimators=500,
+                                        n_estimators=self.estimators,
                                         bootstrap=True,
-                                        min_samples_leaf=10)
+                                        min_samples_leaf=self.sample_per_leaf)
             rforest_model.fit(exams_training, y_training)
             
-            p = rforest_model.predict_proba(exams_test_set)[:,1]
-            print('AUC= ', roc_auc_score(y_test_set,p))
+            y_pred = rforest_model.predict_proba(exams_test_set)
+            # print('AUC= ', roc_auc_score(y_test_set,y_pred))
+        self.data_for_plot = [y_test_set, y_pred]
+        self.data_for_shap = [rforest_model, exams_training]
 
     def encode_fill_NaN(self, training, test_set):
         enc = OrdinalEncoder()
